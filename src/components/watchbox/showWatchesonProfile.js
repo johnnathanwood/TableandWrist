92806a625429fa3cd20b41e287fbe055639488fe
John Wood <johnnathanwood@gmail.com>
2018-12-12 10:35:10 -0600

    fixed issue where watches on profile were rendering all watches in database. Now user will only see ones watch collection.

--- src/components/watchbox/showWatchesonProfile.js
+++ src/components/watchbox/showWatchesonProfile.js
@@ -10,40 +10,37 @@ export default class ProfileCollection extends Component {
     findUserId = () => {
         return localStorage.getItem("credentials")
     }
-
     render() {
+        const credentials = JSON.parse(localStorage.getItem('credentials'))  
         return (
             <React.Fragment>
-            {
-                this.props.watches.map(watches =>
-                    <div id={`watches--${watches.id}`} key={watches.id} className="MessageCard">
-                        <Message floating>
-                            <Comment.Group size='large'>
-                                <Comment>
-                                    <Comment.Content>
-                                        <Comment.Metadata>
-                                            <div className="profile-image"><img src={watches.uploadedFileCloudinaryUrl} alt="sample47" width="300" crop="scale" /></div>
-                                        </Comment.Metadata>
-                                        <Comment.Text>
-                                            <div>Brand: {watches.brand}</div>
-                                        </Comment.Text>
-                                        <Comment.Text>
-                                            <p>Model: {watches.model}</p>
-                                        </Comment.Text>
-                                        <Comment.Text>
-                                            <p>Year: {watches.year}</p>
-                                        </Comment.Text>
-                                        <Comment.Text>
-                                            <p>Price: ${watches.price}</p>
-                                        </Comment.Text>
-                                    </Comment.Content>
-                                </Comment>
-                            </Comment.Group>
-                        </Message>
-                    </div>
-                )
-            }
-        </React.Fragment>
+                <h1>{credentials.username}'s watch collection</h1>
+                        <div id={`watch--${this.props.watch.id}`} key={this.props.watch.id} className="MessageCard">
+                            <Message floating>
+                                <Comment.Group size='large'>
+                                    <Comment>
+                                        <Comment.Content>
+                                            <Comment.Metadata>
+                                                <div className="profile-image"><img src={this.props.watch.uploadedFileCloudinaryUrl} alt="sample47" width="300" crop="scale" /></div>
+                                            </Comment.Metadata>
+                                            <Comment.Text>
+                                                <div>Brand: {this.props.watch.brand}</div>
+                                            </Comment.Text>
+                                            <Comment.Text>
+                                                <p>Model: {this.props.watch.model}</p>
+                                            </Comment.Text>
+                                            <Comment.Text>
+                                                <p>Year: {this.props.watch.year}</p>
+                                            </Comment.Text>
+                                            <Comment.Text>
+                                                <p>Price: ${this.props.watch.price}</p>
+                                            </Comment.Text>
+                                        </Comment.Content>
+                                    </Comment> 
+                                </Comment.Group>
+                            </Message>
+                        </div>
+            </React.Fragment>
 
         )
     }
